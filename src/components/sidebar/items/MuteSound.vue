1ace1f84570c8467e0d4858d68255e2c123fcfbd
Ricardo Cino <2735602+cino@users.noreply.github.com>
2020-06-09 20:57:27 +0200

    Persist sound setting

--- src/components/sidebar/items/MuteSound.vue
+++ src/components/sidebar/items/MuteSound.vue
@@ -4,6 +4,7 @@
 
 <script>
 import BottomTab from "../tabs/BottomTab";
+import settings from "electron-settings";
 
 const soundOff = "static/serviceIcons/volume_off-24px.svg";
 const soundOn = "static/serviceIcons/volume_up-24px.svg";
@@ -19,15 +20,16 @@ export default {
             icon: null
         };
     },
-    created: function() {
-        this.icon = soundOff;
+    created: async function() {
+        const soundMuted = await settings.get("soundMuted");
+
+        this.icon = soundMuted ? soundOff : soundOn;
     },
     methods: {
         toggleSound() {
             this.icon = this.icon === soundOff ? soundOn : soundOff;
 
-            // Todo actually persist sound toggled
-            console.log("sound togggled");
+            settings.set("soundMuted", this.icon === soundOff);
         }
     }
 };
