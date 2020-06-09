90d166e8b0bc2f2bde71213f04fc66d573d50ad9
Ricardo Cino <2735602+cino@users.noreply.github.com>
2020-06-09 20:49:08 +0200

    Keep track of window state / size

--- src/main.js
+++ src/main.js
@@ -1,20 +1,24 @@
 const { app, BrowserWindow } = require('electron');
+import settings from 'electron-settings';
 
+async function createWindow() {
+    const mainWindowStateKeeper = await windowStateKeeper('main');
 
-function createWindow() {
     // Create the browser window.
     const window = new BrowserWindow({
         titleBarStyle: process.platform === 'darwin' ? 'hidden' : '',
         title: 'Basket',
-        height: 680,
-        width: 900,
-        minHeight: 680,
-        minWidth: 900,
+        x: mainWindowStateKeeper.x,
+        y: mainWindowStateKeeper.y,
+        width: mainWindowStateKeeper.width,
+        height: mainWindowStateKeeper.height,
         webPreferences: {
             nodeIntegration: true,
             webviewTag: true,
         },
     });
+    // Track window state
+    mainWindowStateKeeper.track(window);
 
     // if (isDevMode) {
     window.webContents.openDevTools();
@@ -37,3 +41,48 @@ app.on('window-all-closed', () => {
         app.quit()
     }
 })
+
+async function windowStateKeeper(windowName) {
+    let window, windowState;
+
+    async function setBounds() {
+        // Restore from settings
+        if (await settings.has(`windowState.${windowName}`)) {
+            windowState = await settings.get(`windowState.${windowName}`);
+            return;
+        }
+        // Default
+        windowState = {
+            x: undefined,
+            y: undefined,
+            width: 960,
+            height: 680,
+        };
+    }
+
+    function saveState() {
+        if (!windowState.isMaximized) {
+            windowState = window.getBounds();
+        }
+        windowState.isMaximized = window.isMaximized();
+        settings.set(`windowState.${windowName}`, windowState);
+    }
+
+    function track(win) {
+        window = win;
+        ['resize', 'move', 'close'].forEach(event => {
+            win.on(event, saveState);
+        });
+    }
+
+    await setBounds();
+
+    return ({
+        x: windowState.x,
+        y: windowState.y,
+        width: windowState.width,
+        height: windowState.height,
+        isMaximized: windowState.isMaximized,
+        track,
+    });
+}
