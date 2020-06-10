122e69387e5aa2bb32860f8f440607e8fa3e3398
Ricardo Cino <2735602+cino@users.noreply.github.com>
2020-06-10 22:27:15 +0200

    Create seperate Tab component

--- src/components/sidebar/tabs/BottomTab.vue
+++ src/components/sidebar/tabs/BottomTab.vue
@@ -1,12 +1,15 @@
 <template>
-    <li class="sidebar-item small" :key="identifier" @click="click">
-        <img class="sidebar-logo" :src="icon" />
-    </li>
+    <Tab :identifier="identifier" :icon="icon" :click="click" />
 </template>
 
 <script>
+import Tab from "./Tab";
+
 export default {
     name: "BottomTab",
+    components: {
+        Tab
+    },
     props: {
         icon: {
             required: true,
@@ -24,49 +27,6 @@ export default {
 </script>
 
 <style lang="scss">
-.sidebar-item {
-    margin-left: auto;
-    margin-right: auto;
-    padding: 8px;
-    list-style-type: none;
-
-    &.active {
-        background-color: $bright-gray;
-    }
-
-    &:hover {
-        background-color: $bright-gray;
-        cursor: pointer;
-    }
-
-    &.small {
-        padding-top: 4px;
-        padding-bottom: 4px;
-    }
-
-    .sidebar-logo {
-        margin-left: 4px;
-        padding: 8px;
-        height: 24px;
-        width: 32px;
-        fill: $silver;
-    }
-
-    &.disabled {
-        .sidebar-logo {
-            filter: grayscale(100%);
-        }
-
-        &:hover {
-            cursor: default;
-        }
-    }
-
-    &.is-reordering {
-        z-index: 99999;
-    }
-}
-
 .bottom .sidebar-item .sidebar-logo {
     height: 24px;
 }
---
+++ src/components/sidebar/tabs/Tab.vue
@@ -0,0 +1,69 @@
+<template>
+    <li class="sidebar-item small" :key="identifier" @click="click">
+        <img class="sidebar-logo" :src="icon" />
+    </li>
+</template>
+
+<script>
+export default {
+    name: "Tab",
+    props: {
+        icon: {
+            required: true,
+            type: String
+        },
+        identifier: {
+            required: true,
+            type: String
+        },
+        click: {
+            required: true
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+.sidebar-item {
+    margin-left: auto;
+    margin-right: auto;
+    padding: 8px;
+    list-style-type: none;
+
+    &.active {
+        background-color: $bright-gray;
+    }
+
+    &:hover {
+        background-color: $bright-gray;
+        cursor: pointer;
+    }
+
+    &.small {
+        padding-top: 4px;
+        padding-bottom: 4px;
+    }
+
+    .sidebar-logo {
+        margin-left: 4px;
+        padding: 8px;
+        height: 24px;
+        width: 32px;
+        fill: $silver;
+    }
+
+    &.disabled {
+        .sidebar-logo {
+            filter: grayscale(100%);
+        }
+
+        &:hover {
+            cursor: default;
+        }
+    }
+}
+
+.bottom .sidebar-item .sidebar-logo {
+    height: 24px;
+}
+</style>
