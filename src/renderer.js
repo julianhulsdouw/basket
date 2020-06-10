6d7478e0679db8da604a66e56a4a9ba05f1da653
Ricardo Cino <2735602+cino@users.noreply.github.com>
2020-06-10 22:55:17 +0200

    Setup vue-i18n

--- src/renderer.js
+++ src/renderer.js
@@ -1,10 +1,12 @@
 import App from './components/App'
 import Vue from 'vue';
+import VueI18n from 'vue-i18n'
 import Vuex from 'vuex';
 import store from './store'
 import { mapActions } from "vuex";
 
 Vue.use(Vuex);
+Vue.use(VueI18n);
 
 new Vue({
     created: async function () {
@@ -17,5 +19,13 @@ new Vue({
     },
     components: { App },
     template: '<App/>',
-    store
+    store,
+    i18n: new VueI18n({
+        locale: 'nl',
+        fallbackLocale: 'en',
+        messages: {
+            en: require('./resources/lang/en.json'),
+            nl: require('./resources/lang/nl.json')
+        }
+    }),
 }).$mount('#app');
