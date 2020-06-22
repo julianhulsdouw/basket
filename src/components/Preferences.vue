ec2ecba706701ed0d4ac2f74c3b3ce8450415e71
Ricardo Cino <2735602+cino@users.noreply.github.com>
2020-06-22 19:10:39 +0200

    Start preferences panel

--- src/components/Preferences.vue
+++ src/components/Preferences.vue
@@ -1,6 +1,10 @@
 <template>
     <div v-if="showPreferences" class="view view-active">
-        <h2>Preferences. TODO</h2>
+        <div class="menu">
+            <a href="#" class="active">services</a>
+            <a href="#">settings</a>
+            <a href="#">theme?</a>
+        </div>
     </div>
 </template>
 
@@ -15,3 +19,36 @@
         },
     };
 </script>
+
+<style lang="scss" scoped>
+    .menu {
+        display: flex;
+
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 52px;
+        background-color: $blue-stone;
+
+        a {
+            padding: 8px;
+            font-family: sans-serif;
+            color: $gallery;
+            text-decoration: none;
+            background-color: #196773;
+            border-radius: 8px;
+
+            &:not(:first-child) {
+                margin-left: 8px;
+            }
+
+            &:hover {
+                background-color: #008397;
+            }
+
+            &.active {
+                background-color: #327883;
+            }
+        }
+    }
+</style>
