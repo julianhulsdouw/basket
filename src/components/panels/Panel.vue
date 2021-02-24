0e64156b6ef2314d08d70a7ed0a6edecf59907a3
Ricardo Cino <2735602+cino@users.noreply.github.com>
2021-02-24 23:16:47 +0100

    Extra windows/linux styling changes

--- src/components/panels/Panel.vue
+++ src/components/panels/Panel.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="panel-wrapper">
-        <div class="top">
+        <div class="top" :class="{ mac: isMacOs }">
             <span>{{ title }}</span>
         </div>
 
@@ -9,6 +9,8 @@
 </template>
 
 <script>
+    import { isMac } from '../../library/environment';
+
     export default {
         name: 'Panel',
         props: {
@@ -17,9 +19,11 @@
                 type: String,
             },
         },
-        components: {},
-        computed: {},
-        methods: {},
+        data() {
+            return {
+                isMacOs: isMac,
+            };
+        },
     };
 </script>
 
@@ -32,16 +36,24 @@
 
         .top {
             width: 100%;
-            height: 52px;
+            height: 26px;
             background-color: $blue-stone;
 
             span {
                 position: absolute;
-                padding-top: 25px;
+                padding-top: 2px;
                 margin-left: 10px;
                 font-family: 'Lato', sans-serif;
                 font-size: 16px;
                 color: $gallery;
+            }
+        }
+
+        .mac {
+            height: 52px;
+
+            span {
+                padding-top: 25px;
             }
         }
     }
